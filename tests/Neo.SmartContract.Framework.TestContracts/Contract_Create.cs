7d73aee05f4cc85708699160f39140c0e745607e
EpicChain <114784254+epic-chain@users.noreply.github.com>
2024-02-09 09:37:53 +0100

    Add files via upload

---
+++ tests/Neo.SmartContract.Framework.TestContracts/Contract_Create.cs
@@ -0,0 +1,25 @@
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace Neo.SmartContract.Framework.UnitTests.TestClasses
+{
+    public class Contract_Create : SmartContract
+    {
+        public static string OldContract()
+        {
+            return ContractManagement.GetContract(Runtime.ExecutingScriptHash).Manifest.Name;
+        }
+
+        public static Contract GetContractById(int id)
+        {
+            return ContractManagement.GetContractById(id);
+        }
+
+        public static object GetContractHashes()
+        {
+            var iter = ContractManagement.GetContractHashes();
+            iter.Next();
+            return iter.Value.Item2;
+        }
+    }
+}
