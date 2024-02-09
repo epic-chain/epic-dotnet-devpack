cb7c2b84457f87f71106493b9818e4601d0cf0c4
EpicChain <114784254+epic-chain@users.noreply.github.com>
2024-02-09 09:41:58 +0100

    Add files via upload

---
+++ tests/Neo.SmartContract.Framework.UnitTests/ManifestExtraTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neo.SmartContract.Framework.UnitTests
+{
+    [TestClass]
+    public class ManifestExtraTest
+    {
+        [TestMethod]
+        public void TestExtraAttribute()
+        {
+            var testengine = new TestEngine.TestEngine();
+            testengine.AddEntryScript(Utils.Extensions.TestContractRoot + "Contract_ExtraAttribute.cs");
+
+            var extra = testengine.Manifest.Extra;
+
+            Assert.AreEqual("Neo", extra["Author"].GetString());
+            Assert.AreEqual("dev@neo.org", extra["E-mail"].GetString());
+        }
+    }
+}
